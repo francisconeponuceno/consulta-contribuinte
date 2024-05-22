3e6edfe211554c830902e7dc959f258004e7039e
francisconeponuceno <120109535+francisconeponuceno@users.noreply.github.com>
2024-05-22 14:00:56 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const insEstadual = document.getElementById('insEstadual');
 
+
 function exibir(){
-    insEstadual.classList.add('exibir',)
+    insEstadual.classList.add('exibir');
+}
+
+function outras(){
+    insEstadual.classList.add('ocutar');
 }
